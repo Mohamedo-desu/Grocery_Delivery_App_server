17d691910f56dae8819b1ce59f27c758a9409896
Mohamedo-desu <omkahub@gmail.com>
2025-02-17 13:02:37 +0300

    added models

---
+++ src/models/index.js
@@ -0,0 +1,21 @@
+import BranchModel from "./BranchModel.js";
+import CategoryModel from "./CategoryModel.js";
+import CounterModel from "./CounterModel.js";
+import OrderModel from "./OrderModel.js";
+import ProductModel from "./ProductModel.js";
+import {
+  AdminModel,
+  CustomerModel,
+  DeliveryPartnerModel,
+} from "./UserModel.js";
+
+export {
+  AdminModel,
+  BranchModel,
+  CategoryModel,
+  CounterModel,
+  CustomerModel,
+  DeliveryPartnerModel,
+  OrderModel,
+  ProductModel,
+};
